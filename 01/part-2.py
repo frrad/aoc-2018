beb92ab79ba07059e8353c76d41ca8b696d4a401
Frederick Robinson <frederick.robinson@gmail.com>
2018-12-04 06:07:02 +0000

    Solved 1b

---
+++ 01/part-2.py
@@ -0,0 +1,29 @@
+with open('input-1.txt','r') as f:
+    data = [l.strip() for l in f.readlines()]
+
+inted = []
+
+for x in data:
+    y = int(x[1:])
+    if x[0] == '-':
+        y *=-1
+        
+    inted.append(y)
+
+
+
+curr =   0
+seen = set()
+i = 0
+length = len(inted)
+
+while True:
+    if curr in seen:
+        print curr
+        break
+
+    seen.add(curr)
+    curr += inted[i%length]
+    i+=1
+
+
