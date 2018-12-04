1ac2a8837c1f6f6effae19fba616c1c992a9a3c7
Fred Robinson <frederick.robinson@gmail.com>
2018-12-04 12:54:22 -0800

    autopep8

--- 01/part-1.py
+++ 01/part-1.py
@@ -1,11 +1,11 @@
-with open('input-1.txt','r') as f:
+with open('input-1.txt', 'r') as f:
     data = [l.strip() for l in f.readlines()]
 
 ans = 0
 for x in data:
     y = int(x[1:])
     if x[0] == '-':
-        y *=-1
+        y *= -1
     ans += y
 
 print ans
--- 01/part-2.py
+++ 01/part-2.py
@@ -1,4 +1,4 @@
-with open('input-1.txt','r') as f:
+with open('input-1.txt', 'r') as f:
     data = [l.strip() for l in f.readlines()]
 
 inted = []
@@ -6,13 +6,12 @@ inted = []
 for x in data:
     y = int(x[1:])
     if x[0] == '-':
-        y *=-1
-        
+        y *= -1
+
     inted.append(y)
 
 
-
-curr =   0
+curr = 0
 seen = set()
 i = 0
 length = len(inted)
@@ -23,7 +22,5 @@ while True:
         break
 
     seen.add(curr)
-    curr += inted[i%length]
-    i+=1
-
-
+    curr += inted[i % length]
+    i += 1
